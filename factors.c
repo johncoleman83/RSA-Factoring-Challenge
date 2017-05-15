a812c000fd0458079c222827ea8a918506f90067
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-15 06:24:20 +0000

    completed factors rough draft

--- factors.c
+++ factors.c
@@ -18,23 +18,16 @@ int _strlen(char *s)
  * @filename: the file
  * Return: actual number of letters it could read and print
  */
-void read_textfile(const char *filename, char *buffer)
+int read_textfile(const char *filename, char *buffer)
 {
 	int fd;
 	ssize_t letters, letters2;
 
-	letters = _strlen(buffer);
+	letters = BUFSIZE;
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-	if (read(fd, buffer, letters) == -1)
-	{
-		free(buffer);
-		return (0);
-	}
+	read(fd, buffer, letters);
 }
 
 /**
@@ -61,38 +54,94 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 }
 
 /**
- * main - finds factors of input file
- * @argc: arguments count
- * @argv: arguments
+ * _split - splits string by newlines
+ * @buffer: buffer to split to numbers
  *
  * Return: 0 always Success
  */
-int *_split(char *buffer)
+char **_split(char *buffer)
 {
-	int i = 0, j = 0, l = 0;
+	int i = 0, j = 0, numtotal = 0, len, start;
 	char **numbers;
 	char *num;
 
 	while (buffer[i] != '\0')
 	{
 		if (buffer[i] == '\n')
-			l++;
+			numtotal++;
 		i++;
 	}
-	numbers = _calloc(l, sizeof(int));
+	numbers = _calloc(numtotal + 1, sizeof(char *));
 	i = 0;
 	while (buffer[i] != '\0')
 	{
-		for(j = 0; buffer[i] != '\n'; j++)
-			;
-		i = j + 1;
-		num = _calloc(j, sizeof(int));
-		for(j = 0; buffer[j] != '\n'; j++)
-			;
-
-		
+		start = i, len = 0;
+		for(len = 0; buffer[i] != '\n'; i++)
+			len++;
+		num = _calloc(len + 1, sizeof(char));
+		for(i = start, len = 0; buffer[i] != '\n'; i++, len++)
+			num[len] = buffer[i];
+		num[len] = '\0';
+		numbers[j++] = num;
+		i++;
 	}
+	numbers[j] = NULL;
+	return (numbers);
+}
 
+/**
+ * _atoi - returns integer values from string
+ * @s: input string
+ *
+ * Return: will return integer
+ */
+int _atoi(char *s)
+{
+	int result = 0, sign = 0, c;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == '-')
+			sign++;
+		if (s[c] > 47 && s[c] < 58)
+		{
+			while (s[c] > 47 && s[c] < 58)
+				result = result * 10 - (s[c++] - 48);
+			break;
+		}
+	}
+	return (result *= sign % 2 == 0 ? -1 : 1);
+}
+
+/**
+ * print_numbers - returns integer values from string
+ * @s: input string
+ *
+ * Return: will return integer
+ */
+void print_factors(char **numbers)
+{
+	int i = 0, num, p, q;
+
+	while (numbers[i])
+	{
+		num = _atoi(numbers[i]);
+		q = 2;
+		while (1)
+		{
+			p = num / q;
+			if (p * q == num)
+				break;
+			if (q > num / 2)
+			{
+				q = 1, p = num;
+				break;
+			}
+			q++;
+		}
+		printf("%d=%d*%d\n", num, p, q);
+		i++;
+	}
 }
 
 /**
@@ -104,10 +153,11 @@ int *_split(char *buffer)
  */
 int main(int argc, char **argv)
 {
-	char *filename = argv[1], *buffer;
-	int *numbers;
+	char *filename = argv[1], *buffer, **numbers;
 
 	buffer = _calloc(BUFSIZE, sizeof(char));
 	read_textfile(filename, buffer);
 	numbers = _split(buffer);
+	print_factors(numbers);
+	return (0);
 }
--- factors.h
+++ factors.h
@@ -1,6 +1,7 @@
 #ifndef _FACTORS_H_
 #define _FACTORS_H_
 #define BUFSIZE 1024
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,6 +9,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 int _strlen(char *s);
-void read_textfile(const char *filename, char *buffer);
-void *_calloc(unsigned int nmemb, unsigned int size)
+int read_textfile(const char *filename, char *buffer);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void print_factors(char **numbers);
+int _atoi(char *s);
+char **_split(char *buffer);
 #endif
